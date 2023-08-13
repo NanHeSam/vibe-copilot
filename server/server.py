3fc4a23b46e65e15dde4f992bd23393ce0bcb7df
Sam He <samhenan@gmail.com>
2023-08-12 20:25:11 -0700

    make it pretty

--- server/server.py
+++ server/server.py
@@ -33,23 +33,42 @@ def html():
     return """
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latest Image Display</title>
+    <style>
+        /* Styling to set the background black and center the image */
+        body {
+            background-color: black;
+            margin: 0;
+            display: flex;
+            justify-content: center; /* Center horizontally */
+            align-items: center;     /* Center vertically */
+            height: 100vh;           /* Make the body take up the full viewport height */
+        }
+        
+        img {
+            display: block; /* Remove any default spacing below the image */
+        }
+    </style>
     <script>
         function refreshImage() {
             // Assumes some way (e.g., server-side scripting) to get the latest image URL
             const imageUrl = "/image";  // replace with your logic or method to get the latest image
-            document.getElementById("latestImage").src = imageUrl  + "?timestamp=" + new Date().getTime();;  // Append timestamp to prevent caching
+            document.getElementById("latestImage").src = imageUrl + "?timestamp=" + new Date().getTime();  // Append timestamp to prevent caching
             setTimeout(refreshImage, 100);  // Refreshes the image every 0.1 seconds
         }
     </script>
 </head>
+
 <body onload="refreshImage()">
-    <img id="latestImage" src="" alt="Latest Image" width="500">  <!-- Set width as required -->
+    <img id="latestImage" src="" alt="Latest Image" width="500"> <!-- Set width as required -->
 </body>
+
 </html>
+
     """
 
 if __name__ == "__main__":
